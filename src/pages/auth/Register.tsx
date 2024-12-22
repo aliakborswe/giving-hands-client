4b04ef3833d0b08d5a5ad57d3d65dd38722aa97b
mdaliakbor <aliakbor.js@gmail.com>
2024-12-22 12:49:34 +0600

    register page design done

---
+++ src/pages/auth/Register.tsx
@@ -0,0 +1,153 @@
+import Wrapper from "@/components/common/Wrapper";
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { Button } from "@/components/ui/button";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+import { Link } from "react-router";
+
+
+
+const formSchema = z.object({
+  name: z.string().min(2, {
+    message: "Name must be at least 2 characters.",
+  }),
+  email: z.string().email({
+    message: "Please enter a valid email address.",
+  }),
+  photoURL: z.string().url({
+    message: "Please enter a valid URL for the profile photo.",
+  }),
+  password: z
+    .string()
+    .min(6, { message: "Password must be at least 6 characters." })
+    .refine(
+      (value) => /[A-Z]/.test(value), // Must have at least one uppercase letter
+      { message: "Password must contain at least one uppercase letter." }
+    )
+    .refine(
+      (value) => /[a-z]/.test(value), // Must have at least one lowercase letter
+      { message: "Password must contain at least one lowercase letter." }
+    ),
+});
+
+const Register = () => {
+
+  // Define form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      photoURL: "",
+      password: "",
+    },
+  });
+
+   
+
+  return (
+    <div>
+      <Wrapper className='flex flex-col md:flex-row items-center justify-between gap-4 '>
+        <div className='w-full md:w-1/2'>
+          <Form {...form}>
+            <form className='space-y-8 border-t-2 border-primary pt-6'>
+              <FormField
+                control={form.control}
+                name='name'
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder='Enter Your Name'
+                        {...field}
+                        className='border-foreground'
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name='email'
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder='Enter Your email'
+                        {...field}
+                        className='border-foreground'
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name='photoURL'
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Photo URL</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder='Enter Your Photo url'
+                        {...field}
+                        className='border-foreground'
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name='password'
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder='Enter password'
+                        type='password'
+                        {...field}
+                        className='border-foreground'
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <Button type='submit' className='w-full text-white'>
+                Submit
+              </Button>
+            </form>
+          </Form>
+          <p className='mt-8 text-start text-muted-foreground'>
+            Have an account?{" "}
+            <Link
+              to='/login'
+              className='font-semibold leading-6 text-indigo-600 hover:text-indigo-500'
+            >
+              Please Login
+            </Link>
+          </p>
+        </div>
+        <DotLottieReact
+          src='https://lottie.host/297c1a07-a0bb-4c42-a1a3-16a67e20a863/jY4y2I5TLz.lottie'
+          loop
+          autoplay
+          className='w-full md:w-1/2 aspect-square'
+        />
+      </Wrapper>
+    </div>
+  );
+};
+
+export default Register;
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import MainLayout from "@/layouts/MainLayout";
 import ErrorPage from "@/error-page";
+import Register from "@/pages/auth/Register";
 
 const AllRoutes = () => {
     return(
@@ -12,6 +13,7 @@ const AllRoutes = () => {
                     <Route path="posts" element={<h1>Posts</h1>}/>
                     <Route path="profile" element={<h1>Profile</h1>}/>
                     <Route path="about" element={<h1>About</h1>}/>
+                    <Route path="register" element={<Register/>}/>
                 {/* Privet routes */}
                 </Route>
                 {/* error route without header and footer */}
