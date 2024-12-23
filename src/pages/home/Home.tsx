6e28d76a91c647f43a1c6b22be6744d7b1eb15a9
mdaliakbor <aliakbor.js@gmail.com>
2024-12-23 20:42:55 +0600

    Volunteer Needs Now Section done

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,10 @@
+import VolunterNeedNow from "./VolunterNeedNow";
 
 
 const Home = () => {
     return (
-        <div className="text-3xl font-bold text-center text-text">
-            Home page
+        <div>
+            <VolunterNeedNow/>
         </div>
     );
 };
