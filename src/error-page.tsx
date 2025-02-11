a72a5e959d9785de661ad4e8a58a78ccc72d46a7
aliakbordev <aliakbor.js@gmail.com>
2025-02-11 20:18:10 +0600

    update: error page updated

--- src/error-page.tsx
+++ src/error-page.tsx
@@ -1,24 +1,28 @@
 import { Link } from "react-router";
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 
 const ErrorPage = () => {
   return (
-    <div className='w-full h-screen flex justify-center items-center'>
-      <div className='container mx-auto px-2.5 text-center'>
-        <h1 className='mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary'>
-          404
-        </h1>
-        <p className='mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white'>
+    <div className='w-full h-screen'>
+      <div className='container mx-auto px-2.5 flex flex-col justify-center items-center'>
+        <DotLottieReact
+          src='https://lottie.host/eca3cfe9-6e97-4d51-baf9-33d6916039ed/DptL15B3bv.lottie'
+          loop
+          autoplay
+          className='md:w-96 w-80 aspect-square'
+        />
+        <p className='mb-2 text-xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white'>
           Something's missing.
         </p>
-        <p className='mb-4 text-lg font-light text-gray-500 dark:text-gray-400'>
+        <p className='mb-2 text-base font-light text-center text-gray-500 dark:text-gray-400'>
           Sorry, we can't find that page. You'll find lots to explore on the
           home page.{" "}
         </p>
         <Link
           to='/'
-          className='bg-primary py-3 px-6 rounded-[10px] text-white font-bold text-lg'
+          className='bg-[#FF0080] py-2 px-6 rounded-[10px] text-white font-semibold text-base'
         >
-          Back to Homepage
+          Back to Home
         </Link>
       </div>
     </div>
